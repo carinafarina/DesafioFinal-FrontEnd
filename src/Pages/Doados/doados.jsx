01f48bacb7650a38de93244c8da98ef55402753d
Carina Farina <carinafarina2014@gmail.com>
2025-03-28 19:10:39 -0300

    Configurando os campos da pagina  quero doar o metodo post

--- src/Pages/Doados/doados.jsx
+++ src/Pages/Doados/doados.jsx
@@ -7,13 +7,14 @@ export default function Doados(){
     return(
         <section className={Style.boxDoados}>
             <h2>Livros Doados</h2>
-            <section className={Style.boxCard}>
+            <section className={Style.boxBooks}>
                 <article>
                     <img src={ficcao} alt='Capa livro ficção'/>
                     <h3>O Protagronista</h3>
                     <p>Susanne Andrade</p>
                     <p>Ficção</p>
                 </article>
+                            
                 </section>
         </section>
     )
--- src/Pages/QueroDoar/querodoar.jsx
+++ src/Pages/QueroDoar/querodoar.jsx
@@ -1,24 +1,67 @@
 
 import Style  from './querodoar.module.scss'
 import livro from '../../assets/Frame.png'
-
+import axios from 'axios'
+import {useState} from 'react'
 
 
 export default function QueroDoar(){
+
+    const [titulo, setTitulo] = useState("")
+    const [categoria,setCategoria] = useState("")
+    const [autor, setAutor] = useState("")
+    const [imagem_url, setImagem_url] = useState("")
+
+    const enviarDados = async()=>{
+        const urlApi = "https://desafio2-api-livros-vainaweb.onrender.com/doar"
+
+        const dadosEnviar = {
+            titulo,
+            categoria,
+            autor,
+            imagem_url
+        }
+        const envioApi = await axios.post(urlApi, dadosEnviar)
+
+        alert('Livro Enviado!')
+
+        setTitulo("")
+        setCategoria("")
+        setAutor("")
+        setImagem_url("")
+    }
+
+    const capturaTitulo = (e) =>{
+        setTitulo(e.target.value)
+        console.log(e.target.value)
+    }
+    const capturaCategoria = (e) =>{
+        setCategoria(e.target.value)
+        console.log(e.target.value)
+    }
+    const capturaAutor = (e) =>{
+        setAutor(e.target.value)
+        console.log(e.target.value)
+    }
+    const capturaImagem = (e) =>{
+        setImagem_url(e.target.value)
+        console.log(e.target.value)
+    }
+
     return(
         <section className={Style.principal}>
             <section className={Style.container}>
                 <h2>Por favor, preencha o formulário com suas informações e as informações do Livro</h2>
-                <form action=''>
+                <form onSubmit={(e)=>e.preventDefault()}>
                     <div>
                         <img src={livro} alt='Imgem de um livro'/>  
                         <h3>Informações do Livro </h3>                            
                     </div>
-                    <input type='text'placeholder='Título'/>
-                    <input type='text'placeholder='Categoria'/>
-                    <input type='text'placeholder='Autor'/>
-                    <input type='text'placeholder='Link da Imagem'/>
-                    <input className={Style.doar} type='submit' value="Doar"/>
+                    <input type='text'placeholder='Título'onChange={capturaTitulo} value={titulo}/>
+                    <input type='text'placeholder='Categoria'onChange={capturaCategoria} value={categoria}/>
+                    <input type='text'placeholder='Autor'onChange={capturaAutor} value={autor}/>
+                    <input type='text'placeholder='Link da Imagem'onChange={capturaImagem} value={imagem_url}/>
+                    <input className={Style.doar} type='submit' value="Doar" onClick={enviarDados}/>
                 </form>
             </section>
         </section>

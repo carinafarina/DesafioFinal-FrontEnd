6a3610bc4e1c305f3831f499aa34fef7ebd0b28e
Carina Farina <carinafarina2014@gmail.com>
2025-03-31 15:36:34 -0300

    Subindo a inclusao de useState e UseEffect na pagina de livros doados

--- src/Pages/Doados/doados.jsx
+++ src/Pages/Doados/doados.jsx
@@ -1,9 +1,26 @@
 import Style  from './doados.module.scss'
 import ficcao from '../../assets/ficcao.png'
-
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 
 export default function Doados(){
+
+    const [livros, setLivros] = useState ([])
+
+
+    const getLivros = async () => {
+        const response =  await axios.get( "https://desafio2-api-livros-vainaweb.onrender.com/livros")
+        setLivros(response.data)
+
+    }
+
+    useEffect(()=>{
+        getLivros()
+    },[])
+
+    
+
     return(
         <section className={Style.boxDoados}>
             <h2>Livros Doados</h2>
@@ -14,7 +31,15 @@ export default function Doados(){
                     <p>Susanne Andrade</p>
                     <p>Ficção</p>
                 </article>
-                            
+                   {livros.map((item)=>(
+                        <article>
+                            <img src={item.imagem_url} alt=''/>
+                            <h3>{item.titulo}</h3>
+                            <p>{item.categoria}</p>
+                            <p>{item.autor}</p>
+                        </article>
+
+                   ))}         
                 </section>
         </section>
     )
